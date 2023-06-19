b1a775f730cfa17c035676b42d0299b73c3fbf65
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-19 23:57:33 +0200

    edited the route

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,27 @@
 import { z } from "zod";
+
+import { clerkClient, type User } from "@clerk/nextjs/server"
+
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+    return { id: user.id, username: user.username, profilePicture: user.profileImageUrl }
+}
+
 export const postsRouter = createTRPCRouter({
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.post.findMany();
+    getAll: publicProcedure.query(async ({ ctx }) => {
+        const posts = await ctx.prisma.post.findMany({
+            take: 100,
+        });
+
+        const users = (await clerkClient.users.getUserList({
+            userId: posts.map((post) => post.authorId),
+            limit: 100,
+        })).map(filterUserForClient)
+
+        return posts.map((post) => ({
+            post,
+            author: users.find((user) => user.id === post.authorId)
+        }))
     }),
 });

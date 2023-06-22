9fb5b7e600d10f84bb01db0a00c33fafe254a926
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-22 22:57:43 +0200

    implemented privateProcedure

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -4,7 +4,7 @@ import { TRPCError } from "@trpc/server";
 
 import { clerkClient, type User } from "@clerk/nextjs/server"
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure, privateProcedure } from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
     return { id: user.id, username: user.username, profilePicture: user.profileImageUrl }
@@ -33,4 +33,23 @@ export const postsRouter = createTRPCRouter({
             }
         })
     }),
+    create: privateProcedure
+        .input(
+            z.object({
+                content: z.string().min(1).max(280)
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            const authorId = ctx.currentUser.id;
+
+            const post = await ctx.prisma.post.create({
+                data: {
+                    authorId,
+                    content: input.content
+                }
+            })
+            return post
+        })
+
+
 });

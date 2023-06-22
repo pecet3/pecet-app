d21c8dbfef914b8e11d3bc005fcdc46feeaea910
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-22 22:34:19 +0200

    edited context, created new middleware and procedure

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -6,11 +6,12 @@
  * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
  * need to use are documented accordingly near the end.
  */
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from '@trpc/server';
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import superjson from "superjson";
 import { ZodError } from "zod";
 import { prisma } from "~/server/db";
+import { getAuth } from "@clerk/nextjs/server"
 
 /**
  * 1. CONTEXT
@@ -22,21 +23,6 @@ import { prisma } from "~/server/db";
 
 type CreateContextOptions = Record<string, never>;
 
-/**
- * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
- * it from here.
- *
- * Examples of things you may need it for:
- * - testing, so we don't have to mock Next.js' req/res
- * - tRPC's `createSSGHelpers`, where we don't have req/res
- *
- * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
- */
-const createInnerTRPCContext = (_opts: CreateContextOptions) => {
-  return {
-    prisma,
-  };
-};
 
 /**
  * This is the actual context you will use in your router. It will be used to process every request
@@ -44,8 +30,18 @@ const createInnerTRPCContext = (_opts: CreateContextOptions) => {
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = (_opts: CreateNextContextOptions) => {
-  return createInnerTRPCContext({});
+export const createTRPCContext = (opts: CreateNextContextOptions) => {
+
+  const { req } = opts;
+
+  const sesh = getAuth(req)
+
+  const user = sesh.user
+
+  return {
+    prisma,
+    currentUser: user
+  }
 };
 
 /**
@@ -92,3 +88,19 @@ export const createTRPCRouter = t.router;
  * are logged in.
  */
 export const publicProcedure = t.procedure;
+
+const enforceUserIsAuthed = t.middleware(async ({ ctx, next }) => {
+  if (!ctx.currentUser) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    })
+  }
+
+  return next({
+    ctx: {
+      currentUser: ctx.currentUser
+    }
+  })
+})
+
+export const privateProcedure = t.procedure.use(enforceUserIsAuthed)

18fed5cdfe4d169603cbf36a87732866b079edcd
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-26 23:53:06 +0200

    edited route

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,24 +1,11 @@
+import { filterUserForClient } from './../../helpers/filterUserForClient';
 import { z } from "zod";
 
 import { TRPCError } from "@trpc/server";
 
 import { clerkClient, type User } from "@clerk/nextjs/server"
 
-import { createTRPCRouter, publicProcedure, privateProcedure } from "~/server/api/trpc";
-
-import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
-import { Redis } from "@upstash/redis";
-
-// Create a new ratelimiter, that allows 3 requests per 60 seconds
-const ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(3, "1 m"),
-    analytics: true,
-});
-
-const filterUserForClient = (user: User) => {
-    return { id: user.id, username: user.username, profilePicture: user.profileImageUrl }
-}
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const profileRouter = createTRPCRouter({
 
@@ -34,6 +21,6 @@ export const profileRouter = createTRPCRouter({
             })
         }
 
-        return user
+        return filterUserForClient(user)
     })
 });

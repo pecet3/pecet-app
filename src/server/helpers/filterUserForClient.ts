50b753111b4767e310595cd34cd6cbe75c4304e3
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-26 23:52:51 +0200

    separated helper

---
+++ src/server/helpers/filterUserForClient.ts
@@ -0,0 +1,5 @@
+import { type User } from "@clerk/nextjs/server"
+
+export const filterUserForClient = (user: User) => {
+    return { id: user.id, username: user.username, profilePicture: user.profileImageUrl }
+}

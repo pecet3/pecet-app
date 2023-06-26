3c63b7c7716f0c236c31f8df7691b4e4138bece3
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-26 22:22:18 +0200

    implemented Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,16 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider>
+      <Head>
+        <title>pecetApp</title>
+        <meta name="description" content="pecet app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>

479bf5f2e9ac7049c1a5b25b95d37a975f3d1a4a
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-27 23:03:07 +0200

    created the layout

---
+++ src/pages/layout.tsx
@@ -0,0 +1,11 @@
+import { type PropsWithChildren } from "react";
+
+export const PageLayout = (props: PropsWithChildren) => {
+  return (
+    <main className="background flex h-screen items-center justify-center">
+      <div className="h-full w-full overflow-y-scroll border-x border-slate-400 bg-slate-800 md:max-w-2xl">
+        {props.children}
+      </div>
+    </main>
+  );
+};

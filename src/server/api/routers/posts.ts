9e007e97a299e417268ececa7d95d2d078fae206
Jakub Pacewicz <pecet3107@gmail.coim>
2023-07-01 16:14:58 +0200

    added new function

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -8,6 +8,45 @@ import { createTRPCRouter, publicProcedure, privateProcedure } from "~/server/ap
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import type { Post } from "@prisma/client"
+
+const addUserDataToPosts = async (posts: Post[]) => {
+    const userId = posts.map(post => post.authorId)
+
+    const users = (await clerkClient.users.getUserList({
+        userId,
+        limit: 100,
+    })).map(filterUserForClient)
+
+    return posts.map((post) => {
+        const author = users.find((user) => user.id === post.authorId);
+
+        if (!author) {
+            console.error("AUTHOR NOT FOUND", post);
+            throw new TRPCError({
+                code: "INTERNAL_SERVER_ERROR",
+                message: `Author for post not found. POST ID: ${post.id}, USER ID: ${post.authorId}`,
+            });
+        }
+        // if (!author.username) {
+        //   // user the ExternalUsername
+        //   if (!author.externalUsername) {
+        //     throw new TRPCError({
+        //       code: "INTERNAL_SERVER_ERROR",
+        //       message: `Author has no GitHub Account: ${author.id}`,
+        //     });
+        //   }
+        //   author.username = author.externalUsername;
+        // }
+        return {
+            post,
+            author: {
+                ...author,
+                username: author.username ?? "(username not found)",
+            },
+        };
+    });
+}
 
 // Create a new ratelimiter, that allows 3 requests per 60 seconds
 const ratelimit = new Ratelimit({
@@ -76,7 +115,7 @@ export const postsRouter = createTRPCRouter({
             take: 100,
             orderBy: [{ createdAt: "desc" }]
 
-        })
+        }).then(addUserDataToPosts)
         return posts
     }),
     getPostsByUserId: publicProcedure
@@ -93,7 +132,7 @@ export const postsRouter = createTRPCRouter({
                     },
                     take: 100,
                     orderBy: [{ createdAt: "desc" }],
-                })
+                }).then(addUserDataToPosts)
 
             return posts
         }

48d8a7f574f35ce598319de2606fbec4a4091399
Jakub Pacewicz <pecet3107@gmail.coim>
2023-07-03 21:33:59 +0200

    edited the filter

--- src/server/helpers/filterUserForClient.ts
+++ src/server/helpers/filterUserForClient.ts
@@ -1,5 +1,5 @@
 import { type User } from "@clerk/nextjs/server"
 
 export const filterUserForClient = (user: User) => {
-    return { id: user.id, username: user.username, profilePicture: user.profileImageUrl, description: user.publicMetadata.description as string }
+    return { id: user.id, username: user.username, profilePicture: user.profileImageUrl, description: user.publicMetadata.description as string, backgroundImg: user.publicMetadata.backgroundImg as string }
 }

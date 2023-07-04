92d5d5bfadcd03fca7c59a1aaa7259b4f4293afe
Jakub Pacewicz <pecet3107@gmail.coim>
2023-07-04 19:48:52 +0200

    preparing for different ui

--- src/pages/editProfile/index.tsx
+++ src/pages/editProfile/index.tsx
@@ -7,6 +7,10 @@ import { toast } from "react-hot-toast";
 import PageLayout from "../layout";
 import { UploadDropzone, UploadButton } from "~/utils/uploadthing";
 import "@uploadthing/react/styles.css";
+import Head from "next/head";
+import Image from "next/image";
+import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import { BackArrow } from "../../components/backArrow";
 
 const EditProfilePage = () => {
   const { user } = useUser();
@@ -24,6 +28,10 @@ const EditProfilePage = () => {
   }, [input]);
 
   const ctx = api.useContext();
+
+  const { data } = api.profile?.getUserById.useQuery({
+    userId: user?.id as string,
+  });
 
   const { mutate: mutateBackground, isLoading } =
     api.profile.updateBackground.useMutation({
@@ -73,93 +81,188 @@ const EditProfilePage = () => {
       },
     });
   if (!user) return <p>Unauthorized</p>;
+  if (!data) return <p>Error</p>;
   return (
-    <main className="background flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center justify-center rounded-lg bg-slate-700 p-4">
-        <div className="flex flex-col items-center justify-center gap-1">
-          <label
-            htmlFor="description"
-            className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Edit your description
-          </label>
-          <textarea
-            onChange={(e) =>
-              setInput(
-                (prev) => (prev = { ...prev, description: e.target.value })
-              )
+    <>
+      <Head>
+        <title>Edit a profile</title>
+        <meta name="description" content={`pecet app`} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <PageLayout>
+        <div className="relative  flex h-60 flex-col bg-slate-800 p-0 ">
+          <img
+            src={
+              data.backgroundImg ??
+              "https://images2.alphacoders.com/941/thumb-1920-941898.jpg"
             }
-            id="description"
-            rows={4}
-            className="block w-full resize-none rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm
-             text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700
-              dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500
-              dark:focus:ring-blue-500"
-            placeholder={
-              (user.publicMetadata.description as string) ||
-              "write rour description"
-            }
-          ></textarea>
-          <p>
-            {counter}/{maxLength}
-          </p>
-          <div className="flex">
-            <button
-              onClick={() =>
-                mutate({ description: input.description, userId: user?.id })
-              }
-              className="m-auto rounded-md bg-slate-500 px-1"
-            >
-              Submit
-            </button>
+            className="fill h-full bg-slate-800 opacity-50 ring-slate-800 blur-sm"
+            alt="Your profile photo"
+          />
+          <div className="absolute left-1/4 top-36 m-auto flex flex-col items-center justify-center self-start sm:left-8 md:left-2">
+            <Image
+              src={data.profilePicture}
+              className="h-44 w-44 rounded-full bg-slate-700 ring-2 ring-slate-700"
+              alt="Your profile photo"
+              width={48}
+              height={48}
+            />
+            <p className="text-xl font-bold md:text-2xl">@{data.username}</p>
           </div>
+          <BackArrow />
         </div>
-        <div className="flex flex-col items-center justify-center gap-1">
-          <label
-            htmlFor="description"
-            className="my-2 block text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Change your URL Image
-          </label>
-          <div className="flex gap-2">
-            {/* <input
-              type="text"
-              onChange={(e) =>
-                setInput(
-                  (prev) => (prev = { ...prev, backgroundImg: e.target.value })
-                )
-              }
-              id="background-url"
-              className="block w-full resize-none rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm
+        <div className="m-auto mt-24 flex max-w-md p-2 text-sm sm:mb-12 sm:ml-48 sm:mt-4  md:text-base">
+          <div className="flex items-center justify-center gap-1">
+            <div className="m-auto flex flex-col items-center justify-center">
+              <label
+                htmlFor="description"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Edit your description
+              </label>
+              <textarea
+                onChange={(e) =>
+                  setInput(
+                    (prev) => (prev = { ...prev, description: e.target.value })
+                  )
+                }
+                id="description"
+                rows={4}
+                className="block w-56 resize-none rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm
              text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700
               dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500
               dark:focus:ring-blue-500"
-              placeholder="paste a URL "
-            /> */}
-            <div className="flex">
-              <UploadButton
-                endpoint="imageUploader"
-                onClientUploadComplete={(res) => {
-                  // Do something with the response
-                  // console.log("Files: ", res);
-                  // alert("Upload Completed");
-                  if (typeof res === "undefined") return;
-                  const url = res[0]?.fileUrl as string;
-                  mutateBackground({
-                    backgroundImg: url,
-                    userId: user?.id,
-                  });
-                }}
-                onUploadError={(error: Error) => {
-                  // Do something with the error.
-                  alert(`ERROR! ${error.message}`);
-                }}
-              />
+                placeholder={
+                  (user.publicMetadata.description as string) ||
+                  "write rour description"
+                }
+              ></textarea>
+            </div>
+            <div className="flex flex-col gap-1">
+              <p>
+                {counter}/{maxLength}
+              </p>
+              <button
+                onClick={() =>
+                  mutate({ description: input.description, userId: user?.id })
+                }
+                className="m-auto rounded-md bg-slate-500 px-1"
+              >
+                Update
+              </button>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+        <div className="m-auto flex max-w-[14rem] flex-col items-center justify-center gap-1 rounded-lg bg-slate-500 bg-opacity-70 p-2 text-center">
+          <span>Update your Background Image</span>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={(res) => {
+              // Do something with the response
+              // console.log("Files: ", res);
+              // alert("Upload Completed");
+              if (typeof res === "undefined") return;
+              const url = res[0]?.fileUrl as string;
+              mutateBackground({
+                backgroundImg: url,
+                userId: user?.id,
+              });
+            }}
+            onUploadError={(error: Error) => {
+              // Do something with the error.
+              alert(`ERROR! ${error.message}`);
+            }}
+          />
+        </div>
+      </PageLayout>
+    </>
+
+    // <main className="background flex h-screen items-center justify-center">
+    //   <div className="flex flex-col items-center justify-center rounded-lg bg-slate-700 p-4">
+    // <div className="flex flex-col items-center justify-center gap-1">
+    //   <label
+    //     htmlFor="description"
+    //     className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+    //   >
+    //     Edit your description
+    //   </label>
+    //   <textarea
+    //     onChange={(e) =>
+    //       setInput(
+    //         (prev) => (prev = { ...prev, description: e.target.value })
+    //       )
+    //     }
+    //     id="description"
+    //     rows={4}
+    //     className="block w-full resize-none rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm
+    //      text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700
+    //       dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500
+    //       dark:focus:ring-blue-500"
+    //     placeholder={
+    //       (user.publicMetadata.description as string) ||
+    //       "write rour description"
+    //     }
+    //   ></textarea>
+    //   <p>
+    //     {counter}/{maxLength}
+    //   </p>
+    //   <div className="flex">
+    //     <button
+    //       onClick={() =>
+    //         mutate({ description: input.description, userId: user?.id })
+    //       }
+    //       className="m-auto rounded-md bg-slate-500 px-1"
+    //     >
+    //       Submit
+    //     </button>
+    //   </div>
+    // </div>
+    //     <div className="flex flex-col items-center justify-center gap-1">
+    //       <label
+    //         htmlFor="description"
+    //         className="my -2 block text-sm font-medium text-gray-900 dark:text-white"
+    //       >
+    //         Change your URL Image
+    //       </label>
+    //       <div className="flex gap-2">
+    //         <input
+    //           type="text"
+    //           onChange={(e) =>
+    //             setInput(
+    //               (prev) => (prev = { ...prev, backgroundImg: e.target.value })
+    //             )
+    //           }
+    //           id="background-url"
+    //           className="block w-full resize-none rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm
+    //          text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700
+    //           dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500
+    //           dark:focus:ring-blue-500"
+    //           placeholder="paste a URL "
+    //         />
+    //         <div className="flex">
+    // <UploadButton
+    //   endpoint="imageUploader"
+    //   onClientUploadComplete={(res) => {
+    //     // Do something with the response
+    //     // console.log("Files: ", res);
+    //     // alert("Upload Completed");
+    //     if (typeof res === "undefined") return;
+    //     const url = res[0]?.fileUrl as string;
+    //     mutateBackground({
+    //       backgroundImg: url,
+    //       userId: user?.id,
+    //     });
+    //   }}
+    //   onUploadError={(error: Error) => {
+    //     // Do something with the error.
+    //     alert(`ERROR! ${error.message}`);
+    //   }}
+    // />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </main>
   );
 };
 

626919f65d2c005b44642ed4bed9734b5716423f
Jakub Pacewicz <pecet3107@gmail.coim>
2023-07-05 22:34:29 +0200

    changed icon

--- src/components/backArrow.tsx
+++ src/components/backArrow.tsx
@@ -1,11 +1,11 @@
-import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import { BsArrowLeftCircle } from "react-icons/bs";
 import Link from "next/link";
 export const BackArrow = () => {
   return (
-    <Link className="absolute rounded-full p-2 text-slate-800 " href="/">
-      <BsFillArrowLeftCircleFill
+    <Link className="absolute rounded-full p-2 text-slate-50 " href="/">
+      <BsArrowLeftCircle
         size={32}
-        className="rounded-full bg-slate-300 bg-opacity-50 shadow-sm shadow-gray-600"
+        className="rounded-full bg-slate-300 bg-opacity-40 shadow-sm shadow-gray-600"
       />
     </Link>
   );

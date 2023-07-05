effcade5aaa96a9985adb35dfd72a0906e09c9e9
Jakub Pacewicz <pecet3107@gmail.coim>
2023-07-05 22:38:57 +0200

    created thje page

---
+++ src/pages/editProfile/details/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { UserProfile } from "@clerk/nextjs";
+import "@uploadthing/react/styles.css";
+import Head from "next/head";
+
+const EditProfilePage = () => {
+  return (
+    <>
+      <Head>
+        <title>Edit a profile</title>
+        <meta name="description" content={`pecet app`} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="background m-auto flex justify-center">
+        <UserProfile />
+      </main>
+    </>
+  );
+};
+
+export default EditProfilePage;

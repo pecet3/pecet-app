7ce9bcb38269423bc14784a4a4a2d084d178e6b4
Jakub Pacewicz <pecet3107@gmail.coim>
2023-07-08 23:12:12 +0200

    implemented comments

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -20,7 +20,7 @@ const PostPage: NextPage = () => {
 
   const postId = router.query.id?.toString();
 
-  const { isLoaded: userLoaded, isSignedIn } = useUser();
+  const { isLoaded: userLoaded, isSignedIn, user } = useUser();
 
   //start fetching asap
   if (typeof postId === "undefined") return;
@@ -29,11 +29,13 @@ const PostPage: NextPage = () => {
     postId: postId,
   });
 
+  const { mutate: mutateDelete } = api.posts.delete.useMutation({});
+
   if (!data || typeof data === "undefined") return null;
 
   const author = data[0]?.author;
   const post = data[0]?.post;
-
+  const comments = post?.comments;
   if (
     !userLoaded ||
     typeof author === "undefined" ||
@@ -48,39 +50,56 @@ const PostPage: NextPage = () => {
         <meta name="description" content="pecet app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <main className="background flex h-screen items-center justify-center">
-        <div className="flex items-center gap-1 rounded-lg bg-emerald-700 p-2 md:gap-4 md:p-4">
-          <Link
-            href={`/@${author.username}`}
-            className="flex self-start md:self-center "
-          >
-            <Image
-              src={author.profilePicture}
-              alt={`@${author.username}'s avatar`}
-              className="h-12 w-12 rounded-full bg-slate-500 ring-2 ring-slate-400 md:h-48 md:w-48 md:ring-4"
-              height={48}
-              width={48}
-            />
-          </Link>
-          <div className="flex flex-col items-start gap-2 md:gap-4">
-            <div className="text-md flex gap-1 text-cyan-400 md:text-2xl">
-              <span className="font-bold">{`@${author.username}`}</span>
-              <span className="font-thin">{`∙ ${dayjs(
-                post.createdAt
-              ).fromNow()}`}</span>
-              <span className="font-thin">{`- ${post.emoji}`}</span>
+      <PageLayout>
+        <div className="flex items-end border-b p-1">
+          <div className="flex justify-start gap-2 md:p-2">
+            <Link href={`/@${author.username}`} className="">
+              <Image
+                src={author.profilePicture}
+                alt={`@${author.username}'s avatar`}
+                className="h-10 w-10 rounded-full md:h-12 md:w-12"
+                width={48}
+                height={48}
+              />
+            </Link>
+            <div className="flex flex-col">
+              <div className="flex gap-1 text-sm text-slate-300">
+                <Link href={`/@${author.username}`}>
+                  <span className="font-bold">{`@${author.username}`}</span>
+                </Link>
+                <Link href={`/post/${post.id}`}>
+                  <span className="font-thin">{`∙ ${dayjs(
+                    post.createdAt
+                  ).fromNow()}`}</span>
+                </Link>
+                {user?.id === post.authorId ? (
+                  <button
+                    className="text-xs text-gray-500"
+                    onClick={() => {
+                      mutateDelete({ postId: post.id, authorId: author.id });
+                    }}
+                  >
+                    <i className="text-xs font-extralight text-slate-200">{` ∙`}</i>{" "}
+                    ❌delete
+                  </button>
+                ) : null}
+              </div>
+              <div className="max-w-[15rem] grow sm:max-w-md md:max-w-lg">
+                <span className=" break-words text-base md:text-lg">
+                  {post.content}
+                </span>
+                <span className="mx-1 text-xl font-bold text-slate-400">{` ∙ ${
+                  post.emoji ?? "😐"
+                }`}</span>
+              </div>
             </div>
-            <span className="text-md ml-2 max-w-[18rem] break-words md:max-w-xl md:text-4xl">
-              {post.content}
-            </span>
           </div>
         </div>
-       
-      </main> */}
-
-      <PageLayout>
-        <PostView author={author} post={post} />
-        <p>comments</p>
+        <div>
+          {comments?.map((comment) => {
+            return <p key={comment.id}>{comment.content}</p>;
+          })}
+        </div>
       </PageLayout>
     </>
   );

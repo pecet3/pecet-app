cbfbc58815f3e6d87d61771302acff96ac1cdb0c
Jakub Pacewicz <pecet3107@gmail.coim>
2023-07-11 19:56:06 +0200

    edited

--- src/server/helpers/filterUserForClient.ts
+++ src/server/helpers/filterUserForClient.ts
@@ -1,5 +1,12 @@
 import { type User } from "@clerk/nextjs/server"
 
 export const filterUserForClient = (user: User) => {
-    return { id: user.id, username: user.username, profilePicture: user.profileImageUrl, description: user.publicMetadata.description as string, backgroundImg: user.publicMetadata.backgroundImg as string }
-}
+    if (!user || typeof user === "undefined") return
+    return {
+        id: user.id,
+        username: user.username as string,
+        profilePicture: user.profileImageUrl,
+        description: user.publicMetadata.description as string,
+        backgroundImg: user.publicMetadata.backgroundImg as string
+    }
+} 

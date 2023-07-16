95fe1e7048093b304bccc776098366d88ec8075f
Jakub Pacewicz <pecet3107@gmail.coim>
2023-07-16 20:18:24 +0200

    edited view of tile

--- src/components/postView.tsx
+++ src/components/postView.tsx
@@ -36,7 +36,7 @@ export const PostView = ({ post, author }: PostWithUser) => {
     },
   });
   return (
-    <div className="border-b">
+    <div className="m-2 rounded-md  bg-slate-700 shadow-md shadow-slate-500 ">
       <div className="flex items-end">
         <div className="flex justify-start gap-2 px-1 pt-1 md:px-2 md:pt-2">
           <Link href={`/@${author.username}`} className="">
